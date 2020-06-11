208121594511d8da59005a4b9f416419745f2681
yfkesmit <smityfke@gmail.com>
2020-06-11 16:49:16 +0200

    added feature 1 and 2

--- DAR_P2.R
+++ DAR_P2.R
@@ -14,3 +14,40 @@ nrow(unique_queries)
 #laad de library voor het voorbewerken van de tekst
 library(tau)
 
+#feature 1: komen alle zoektermen voor in de productnaam?
+# functie om te berekenen of alle querywoorden in de productnaam voorkomen
+all.queryterms <- function (queries,docs) 
+{
+  n <- length(queries)
+  feature <- vector(length=n)
+  for(i in 1:n){
+    query <- queries[i]
+    document <- docs[i]
+    a <- textcnt(query,method="string",n=1L)
+    b <- textcnt(document,method="string",n=1L)
+    c <- intersect(names(a), names(b))
+    feature[i] <- as.numeric(length(a)==length(c))}
+  feature
+}
+# bereken deze feature op de query_product tabel
+allterms <- all.queryterms(query_product.dat$search_term,query_product.dat$product_title)
+summary(allterms)
+#feature 2: hoeveel procent van de zoektermen komen voor in de product beschrijving?
+#voeg de productbeschrijving samen met de query_product tabel
+description_merged <- merge (product_descriptions.dat, query_product.dat)
+#functie om te berekenen hoeveel querywoorden er in de productnaam voorkomen
+descriptiontermscount <- function (queries,docs) 
+{
+  n <- length(queries)
+  feature <- vector(length=n)
+  for(i in 1:n){
+    query <- queries[i]
+    document <- docs[i]
+    a <- textcnt(query,method="string",n=1L)
+    b <- textcnt(document,method="string",n=1L)
+    c <- intersect(names(a), names(b))
+    feature[i] <- (length(c)/length(a))}
+  feature
+}
+descriptionterms <- descriptiontermscount(description_merged$search_term,description_merged$product_description)
+summary(descriptionterms)
