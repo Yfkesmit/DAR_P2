440ff79fca5b9348fc84fc828154da4879d53654
yfkesmit <35263785+Yfkesmit@users.noreply.github.com>
2020-06-11 14:51:38 +0200

    added R project

---
+++ DAR_P2.R
@@ -0,0 +1,8 @@
+library(readr)
+query_product.dat <- read.csv("query_product.csv",stringsAsFactors=FALSE)
+product_descriptions.dat <- read.csv("product_descriptions.csv",stringsAsFactors=FALSE)
+
+
+
+
+
