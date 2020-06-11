96161b0571bea44d62e53d7cefbd37579c33af13
yfkesmit <smityfke@gmail.com>
2020-06-11 16:03:08 +0200

    beschrijvende statistieken toegevoegd aan R script

--- DAR_P2.R
+++ DAR_P2.R
@@ -1,8 +1,16 @@
-library(readr)
 query_product.dat <- read.csv("query_product.csv",stringsAsFactors=FALSE)
 product_descriptions.dat <- read.csv("product_descriptions.csv",stringsAsFactors=FALSE)
 
+#bereken de beschrijvende statistieken
+summary(query_product.dat$product_title)
+summary(as.factor(query_product.dat$relevance))
 
+unique_products <- query_product.dat[!duplicated(query_product.dat$product_uid), ]
+nrow(unique_products)
 
+unique_queries <- query_product.dat[!duplicated(query_product.dat$search_term), ]
+nrow(unique_queries)
 
+#laad de library voor het voorbewerken van de tekst
+library(tau)
 
